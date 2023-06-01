698391d8d28b046d9a51f3241eb05afcb223db92
Mahbub Nafi <nafi@bluebees.ai>
2023-06-01 13:26:50 +0600

    Work on Purchase

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -37,7 +37,7 @@ const data = defineProps({ products: Object });
                     <!-- Product Dropdown -->
                     <select name="product_id" id="product_id" v-model="form.product_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
                         <option value="">Select a product</option>
-                        <option v-for="(product, id) in data.products" :value="{ id }">{{ product }}</option>
+                        <option v-for="product in data.products" value=>{{ product }}</option>
                     </select>
 
                     <!-- Product price input -->
